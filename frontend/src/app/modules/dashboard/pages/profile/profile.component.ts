522b85d8752235d47ee6a4aa2c44e37225cee613
Lucas Fernandes Roncolato <lferon14@gmail.com>
2023-11-16 00:46:04 +0000

    ADDED FRONTEND filtered possible project invites Only able to invite users to project you are admin on Or projects that they haven't already been invited to

--- frontend/src/app/modules/dashboard/pages/profile/profile.component.ts
+++ frontend/src/app/modules/dashboard/pages/profile/profile.component.ts
@@ -32,7 +32,9 @@ export class ProfileComponent implements OnInit {
       this.user = results.fetchedUser
       this.currentUser = results.currentUser
 
-      this.filteredIsAdminProjects = this.currentUser.projects.filter(project => project.isProjectAdmin)
+      // console.log(this.user)
+      // console.log(this.currentUser)
+      this.filterInviteToProjectsToDisplay()
 
       if (this.user && this.currentUser) {
         this.user.id === this.currentUser.id ? this.isProfileUser = true : this.isProfileUser = false
@@ -45,6 +47,9 @@ export class ProfileComponent implements OnInit {
   }
 
   onInviteUser(project: any) {
+
+    // remove the project from the list of projects they can be invited to
+    this.filteredIsAdminProjects = this.filteredIsAdminProjects.filter((project: { id: any; }) => project.id != project.id)
 
     const invitation = {
       inviteeId: this.user!.id,
@@ -60,6 +65,22 @@ export class ProfileComponent implements OnInit {
     }
 
     this.userService.inviteToProject(invitation).subscribe()
+  }
+
+
+  filterInviteToProjectsToDisplay(): void {
+    // add all projects the currentUser is a part of
+    this.filteredIsAdminProjects = this.currentUser.projects.filter(project => project.isProjectAdmin)
+
+    // const result = this.filteredIsAdminProjects.filter((o1: { id: any; }) => !this.user?.invitations!.some(o2 => o1.id === o2.project.id))
+
+    // check if the currentUser already has an invitation to the projects user can invite them to
+    // if so then remove the project from the list of projects they can be invited to.
+    this.filteredIsAdminProjects = this.filteredIsAdminProjects.filter((currentUserProject: { id: string; }) => {
+
+      return !this.user?.invitations?.some(searchedUserProject => searchedUserProject.project.id === currentUserProject.id)
+
+    })
   }
 
 }
