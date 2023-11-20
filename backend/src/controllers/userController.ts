d45880dbd5515d98ddb57e822a1f749f94b53de9
Lucas Fernandes Roncolato <lferon14@gmail.com>
2023-11-20 01:03:14 +0000

    UPDATED BACKEND invitation response now checks if the user being invited is the current user separated handle invitation function to user controller

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -112,3 +112,50 @@ module.exports.inviteUser_put = async(req: Request, res: Response) => {
         res.status(400).send({'message': 'Failed to send invitation!', 'error': error})
     }
 }
+
+// HANDLE INVITATION REQUEST
+module.exports.invitationResponse_post = async(req: Request, res: Response) => {
+
+    const { response, user, invitationId, project } = req.body
+
+    try {
+
+        // check if the user being invited is the currently signed in user
+        if (!(user.id === res.locals.jwtUser.id)) throw "can't accept an invitation for someone else!"
+
+        const exists = await UserModel.findOne({_id: user.id, 'invitations._id': invitationId})
+
+        // checking if the invitation really exists in the users invitations list
+        if (exists === null) throw 'Could not find invitation in the users invitation list'
+
+        const userAfterDeletedInvitation = await UserModel.findByIdAndUpdate(user.id, { $pull: { invitations: { _id: invitationId } }})
+
+        // check if the user is already part of the project if so just remove the invitation
+        const alreadyPartOfProject = await UserModel.findOne({_id: user.id, 'projects.id': project.id})
+
+        if (alreadyPartOfProject != null) throw 'already part of the project!'
+
+        // if response is true > accepted invitation
+        if (response === true) {
+            
+            project.isProjectAdmin = false
+            user.isProjectAdmin = false
+
+            // add project to user projects
+            const addProjectToUser = await UserModel.findByIdAndUpdate(user.id, { $push: { projects: project }})
+
+            // add the user to the projects members
+            const addUserToProject = await ProjectModel.findByIdAndUpdate(project.id, { $push: { members: user }})
+
+            res.status(200).send({'message': 'Accepted invitation!'})
+    } else {
+        // if response is false > declined invitation
+        res.status(200).send({'message': `declined invitation to ${invitationId}`})
+
+    }
+    } catch(error) {
+        res.status(400).send({'message': 'Failed to accept or decline invitation!', 'error': error})
+    }
+}
+
+
