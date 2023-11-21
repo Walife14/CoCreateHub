06ec9e96f448a1339189e3ba809bf878235ac8d2
Lucas Fernandes Roncolato <lferon14@gmail.com>
2023-11-21 17:42:08 +0000

    testing access control header herokuapp

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ const app = express()
 app.use(express.json())
 
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "https://www.cocreatehub.space/");
+    res.setHeader("Access-Control-Allow-Origin", "https://co-create-hub-8d7609aba189.herokuapp.com/");
     next()
 })
 
