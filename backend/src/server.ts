32c4e12214a14a22167e1514aab0c8abfb714898
Lucas Fernandes Roncolato <lferon14@gmail.com>
2023-11-21 17:37:58 +0000

    testing access control header cocreatehub.space

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ const app = express()
 app.use(express.json())
 
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "www.cocreatehub.space");
     next()
 })
 
