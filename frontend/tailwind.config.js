6bb1f164c492116508fb60e5fbc34cec1d9549c8
Lucas Fernandes Roncolato <lferon14@gmail.com>
2023-12-01 01:48:43 +0000

    FIXED missing relative position in input container the input validation error WAS off-screen due to position ADDED fadein animation, and added to app-card ADDED title to project and favicon

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,7 +15,8 @@ module.exports = {
       },
 
       animation: {
-        float: 'floatAround 8s linear infinite'
+        float: 'floatAround 8s linear infinite',
+        fade: 'fadeIn 0.3s linear'
       },
 
       keyframes: theme => ({
@@ -24,6 +25,13 @@ module.exports = {
           '25%': { transform: 'translate(5px, 2px)' },
           '50%': { transform: 'translate(2px, 3px)' },
           '75%': { transform: 'translate(-3px, -5px)' }
+        },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '25%': { opacity: '0.25'},
+          '50%': { opacity: '0.5'},
+          '75%': { opacity: '0.75'},
+          '100%': {opacity: '1'}
         }
       })
     },
